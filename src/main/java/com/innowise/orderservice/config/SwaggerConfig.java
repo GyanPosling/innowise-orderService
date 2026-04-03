74dcf196fcbdd0c0f22a5204e0d7e0602c4c08de
GyanPosling <okicok578@gmail.com>
2026-04-03 16:04:31 -0400

    tasks/KUBER-0: Fix swagger config issues: replace http/locahost on innowise.local

--- src/main/java/com/innowise/orderservice/config/SwaggerConfig.java
+++ src/main/java/com/innowise/orderservice/config/SwaggerConfig.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,9 +18,21 @@ import org.springframework.context.annotation.Configuration;
         bearerFormat = "JWT"
 )
 public class SwaggerConfig {
+
+    @Value("${app.url:}")
+    private String appUrl;
+
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+
+        if (!appUrl.isBlank()) {
+            openAPI.servers(List.of(new Server()
+                    .url(appUrl)
+                    .description("Public Gateway URL")));
+        }
+
+        return openAPI;
     }
 }
